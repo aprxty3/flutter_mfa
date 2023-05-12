4c5c2bffef111e26ecda9962edfb12003f6e3898
aprxty3 <ajiprxty3@gmail.com>
2023-05-12 10:54:53 +0700

    improve ui and function on home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mfa/screen/register_screen.dart';
+import 'package:go_router/go_router.dart';
+
+import '../utils/helper.dart';
+import 'list_maf_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,6 +11,59 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: _appBar(),
+      body: _contentBody(),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      title: const Text('Home'),
+      actions: [
+        PopupMenuButton(
+          itemBuilder: (context) {
+            return [
+              PopupMenuItem(
+                child: const Text('Unenroll MFA'),
+                onTap: () {
+                  context.push(ListMFAScreen.route);
+                },
+              ),
+              PopupMenuItem(
+                child: const Text('Logout'),
+                onTap: () {
+                  supabase.auth.signOut();
+                  context.go(RegisterScreen.route);
+                },
+              ),
+            ];
+          },
+        )
+      ],
+    );
+  }
+
+  _contentBody() {
+    final privatePostsFuture =
+        supabase.from('private_posts').select<List<Map<String, dynamic>>>();
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: privatePostsFuture,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Center(child: Text(snapshot.error.toString()));
+        }
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        final data = snapshot.data!;
+        return ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text(data[index]['content']));
+          },
+        );
+      },
+    );
   }
 }
